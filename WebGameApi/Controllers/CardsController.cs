353e0c48e22517ddc41426e82709e104ec16a884
Junior gonçalves <decimo.terceiro13@hotmail.com>
2023-02-16 19:42:10 -0700

    0.3

--- WebGameApi/Controllers/CardsController.cs
+++ WebGameApi/Controllers/CardsController.cs
@@ -1,11 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
-
+using Newtonsoft.Json;
+using WebGameApi.Models;
+using XPagedList;
 
 
 namespace WebGameApi.Controllers
 {
+
+
+
     public class CardsController : Controller
     {
+        private readonly string ENDPOINT = "https://api.magicthegathering.io/v1/cards";
+        private readonly HttpClient _httpClient = null;
+        private List<Card> _cards = null;
+
+        public CardsController()
+        {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri(ENDPOINT);
+
+        }
         public IActionResult Index()
         {
             return View();
